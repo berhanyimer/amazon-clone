3208b856680aad17de07a4eefda9054d41283b22
Berhan <berhan92gmail.com>
2024-09-17 00:50:43 -0400

    header css and carousel

--- amazon-clone/src/App.jsx
+++ amazon-clone/src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 
 import "./App.css";
 import Header from "./Components/Header";
+import CarouselEffect from "./Components/Carousel/Carousel";
 
 function App() {
   return (
     <>
       <Header />
+      <CarouselEffect />
     </>
   );
 }
--- amazon-clone/src/Components/LowerHeader.jsx
+++ amazon-clone/src/Components/LowerHeader.jsx
@@ -1,23 +1,21 @@
 import React from "react";
 import { AiOutlineMenu } from "react-icons/ai";
-import classes from "../Components/header.module.css"
+import classes from "../Components/Header.module.css"
 function LowerHeader() {
   return (
-    <>
-      <div className={classes.lower_container}>
-        <ul>
-          <li>
-            <AiOutlineMenu />
-          </li>
-          <li>All</li>
-          <li>Today's Deals</li>
-          <li>Customer Service</li>
-          <li>Registry</li>
-          <li>Gift Cards</li>
-          <li>Sell</li>
-        </ul>
-      </div>
-    </>
+    <div className= {classes.lower_container}>
+      <ul>
+        <li>
+          <AiOutlineMenu />
+        </li>
+        <li>All</li>
+        <li>Today's Deals</li>
+        <li>Customer Service</li>
+        <li>Registry</li>
+        <li>Gift Cards</li>
+        <li>Sell</li>
+      </ul>
+    </div>
   );
 }
 
